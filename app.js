1441812156927333f2740c70ad5ac7aea55cb708
wojtekasus1 <wojtekasus1@o2.pl>
2023-10-15 00:38:28 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const { hostname } = require('os')
+const port = 3000
+
+const generateTitle = () =>{
+    return 'Nodejs'
+}
+
+const app = express()
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+
+const title = generateTitle()
+
+    res.render('index',{
+        pageTittle: title,
+        pagebody: 'hi node'
+    })
+})
+
+app.get('/kontakt', (req, res) => {
+    res.send('Dane kontaktowe')
+})
+app.listen(port)
